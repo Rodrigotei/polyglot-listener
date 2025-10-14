dcc0b9669940f40c541eaa89d670d397b712c757
Rodrigo Teixeira Oliveira <rodrigoteixeira.oli0@gmail.com>
2025-10-13 22:21:27 -0300

    updated name and logo

--- src/components/app-bar.vue
+++ src/components/app-bar.vue
@@ -1,5 +1,5 @@
 <template>
     <v-app-bar class="bg-primary">
-        <v-app-bar-title class="text-center">PhrasesAI</v-app-bar-title>
+        <v-app-bar-title class="text-center">Polyglot Listener</v-app-bar-title>
     </v-app-bar>
 </template>
